ab1433d38df26a7c8b606248fd1b1d0d26187c81
Yan <rewert60@gmail.com>
2015-11-13 00:14:05 +0200

    The method of delivery and contact information

--- cart.php
+++ cart.php
@@ -17,6 +17,32 @@
         $delete = mysql_query("DELETE FROM cart WHERE cart_id = '$id' and cart_ip = '{$_SERVER['REMOTE_ADDR']}'", $link);
         break;
     }
+    
+    if (isset($_POST["submitdata"]))
+    {
+        $_SESSION["order_delivery"] = $_POST["order_delivery"];
+        $_SESSION["order_fio"] = $_POST["order_fio"];
+        $_SESSION["order_email"] = $_POST["order_email"];
+        $_SESSION["order_phone"] = $_POST["order_phone"];
+        $_SESSION["order_address"] = $_POST["order_address"];
+        $_SESSION["order_note"] = $_POST["order_note"];
+        
+        header("Location: cart.php?action=completion");
+    }
+    
+    $result = mysql_query("SELECT * FROM cart,table_products WHERE cart.cart_ip = '{$_SERVER['REMOTE_ADDR']}' AND table_products.products_id = cart.cart_id_product", $link);
+    if (mysql_num_rows($result) > 0)
+    {
+        $row = mysql_fetch_array($result);
+        
+        do 
+        {
+            $int = $int + ($row["price"] * $row["cart_count"]);
+        }
+        while ($row = mysql_fetch_array($result));
+        
+        $itogpricecart = $int;
+    }
 
 ?>
 <!DOCTYPE HTML>
@@ -168,16 +194,59 @@
         <div id="block-step">        
         <div id="name-step">        
         <ul>
-        <li><a>1. Корзина товаров</a></li>
+        <li><a href="cart.php?action=oneclick">1. Корзина товаров</a></li>
         <li><span>&rarr;</span></li>
         <li><a class="active">2. Контактная информация</a></li>
         <li><span>&rarr;</span></li>
         <li><a>3. Завершение</a></li>                
         </ul>        
         </div>        
-        <p>шаг 1 из 3</p>
-        <a href="cart.php?action=clear">Очистить</a>        
+        <p>шаг 2 из 3</p>               
         </div>        
+        ';
+        
+        $chck = "";
+        if ($_SESSION['order_delivery'] == "По почте") $chck1 = "checked";
+        if ($_SESSION['order_delivery'] == "Курьером") $chck2 = "checked";
+        if ($_SESSION['order_delivery'] == "Самовывоз") $chck3 = "checked";
+        
+        echo '
+        
+        <h3 class="title-h3">Способы доставки:</h3>
+        <form method="post">
+        <ul id="info-radio">
+        <li>
+        <input type="radio" name="order_delivery" class="order_delivery" id="order_delivery1" value="По почте" '.$chck1.' />
+        <label class="label_delivery" for="order_delivery1">По почте</label>
+        </li>
+        <li>
+        <input type="radio" name="order_delivery" class="order_delivery" id="order_delivery2" value="Курьером" '.$chck2.' />
+        <label class="label_delivery" for="order_delivery2">Курьером</label>
+        </li>
+        <li>
+        <input type="radio" name="order_delivery" class="order_delivery" id="order_delivery3" value="Самовывоз" '.$chck3.' />
+        <label class="label_delivery" for="order_delivery3">Самовывоз</label>
+        </li>       
+        </ul>
+        <h3 class="title-h3" >Информация для доставки:</h3>
+        <ul id="info-order">
+        ';
+        
+        if ($_SESSION['auth'] != 'yes_auth')
+        {
+            echo '
+            
+            <li><label for="order_fio"><span>*</span>ФИО</label><input type="text" name="order_fio" id="order_fio" value="'.$_SESSION["order_fio"].'" /><span class="order_span_style">Пример: Иванов Иван Иванович</span></li>
+            <li><label for="order_email"><span>*</span>E-mail</label><input type="text" name="order_email" id="order_email" value="'.$_SESSION["order_email"].'" /><span class="order_span_style" >Пример: ivanov@mail.ru</span></li>
+            <li><label for="order_phone"><span>*</span>Телефон</label><input type="text" name="order_phone" id="order_phone" value="'.$_SESSION["order_phone"].'" /><span class="order_span_style" >Пример: 8 950 100 12 34</span></li>
+            <li><label class="order_label_style" for="order_address"><span>*</span>Адрес<br /> доставки</label><input type="text" name="order_address" id="order_address" value="'.$_SESSION["order_address"].'" /><span>Пример: г. Харьков,<br /> пр. Л. Свободы д 51а, к 305</span></li>            
+            ';
+        }
+        echo '
+        <li><label class="order_label_style" for="order_note">Примечание</label><textarea name="order_note" >'.$_SESSION["order_note"].'</textarea><span>Уточните информацию о заказе</span></li>
+        </ul>
+        <p align="right"><input type="submit" name="submitdata" id="confirm-button-next" value="Далее" /></p>
+        </form>
         ';
         
         break;
@@ -188,17 +257,47 @@
         <div id="block-step">        
         <div id="name-step">        
         <ul>
-        <li><a>1. Корзина товаров</a></li>
+        <li><a href="cart.php?action=oneclick">1. Корзина товаров</a></li>
         <li><span>&rarr;</span></li>
-        <li><a>2. Контактная информация</a></li>
+        <li><a href="cart.php?action=confirm">2. Контактная информация</a></li>
         <li><span>&rarr;</span></li>
         <li><a class="active">3. Завершение</a></li>                
         </ul>        
         </div>        
-        <p>шаг 1 из 3</p>
-        <a href="cart.php?action=clear">Очистить</a>        
-        </div>        
+        <p>шаг 3 из 3</p>                
+        </div> 
+        <h3>Конечная информация</h3>       
         ';
+        
+        if ($_SESSION["auth"] == 'yes_auth')
+        {
+            echo '
+            <ul id="list-info">
+            <li><strong>Способ доставки: </strong>'.$_SESSION['order_delivery'].'</li>
+            <li><strong>Email: </strong>'.$_SESSION['auth_email'].'</li>
+            <li><strong>ФИО: </strong>'.$_SESSION['auth_surname'].' '.$_SESSION['auth_name'].' '.$_SESSION['auth_patronymic'].'</li>
+            <li><strong>Адрес доставки: </strong>'.$_SESSION['auth_address'].'</li>
+            <li><strong>Телефон: </strong>'.$_SESSION['auth_phone'].'</li>
+            <li><strong>Примечание: </strong>'.$_SESSION['order_note'].'</li>            
+            </ul>
+            ';
+        }else{
+            echo '
+           <ul id="list-info">
+            <li><strong>Способ доставки: </strong>'.$_SESSION['order_delivery'].'</li>
+            <li><strong>Email: </strong>'.$_SESSION['order_email'].'</li>
+            <li><strong>ФИО: </strong>'.$_SESSION['order_fio'].'</li>
+            <li><strong>Адрес доставки: </strong>'.$_SESSION['order_address'].'</li>
+            <li><strong>Телефон: </strong>'.$_SESSION['order_phone'].'</li>
+            <li><strong>Примечание: </strong>'.$_SESSION['order_note'].'</li>            
+            </ul>
+            ';
+        }
+        
+        echo '
+            <h2 class="itog-price" align="right">Итого: <strong>'.$itogpricecart.'</strong> грн</h2>
+            <p align="right" class="button-next" ><a href="">Оплатить</a></p>            
+            ';
         
         break;
         
--- js/shop-script.js
+++ js/shop-script.js
@@ -326,4 +326,69 @@ $('#input-search').bind('textchange', function() {
     }
 });
 
+    //Шаблон проверки email на правильность
+    function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    return pattern.test(emailAddress);
+    }
+ // Контактные данные
+  $('#confirm-button-next').click(function(e){   
+
+   var order_fio = $("#order_fio").val();
+   var order_email = $("#order_email").val();
+   var order_phone = $("#order_phone").val();
+   var order_address = $("#order_address").val();
+   
+ if (!$(".order_delivery").is(":checked"))
+ {
+    $(".label_delivery").css("color","#E07B7B");
+    send_order_delivery = '0';
+
+ }else { $(".label_delivery").css("color","black"); send_order_delivery = '1';
+
+  
+  // Проверка ФИО 
+ if (order_fio == "" || order_fio.length > 50 )
+ {
+    $("#order_fio").css("borderColor","#FDB6B6");
+   send_order_fio = '0';
+   
+ }else { $("#order_fio").css("borderColor","#DBDBDB");  send_order_fio = '1';}
+
+  
+ //проверка email
+ if (isValidEmailAddress(order_email) == false)
+ {
+    $("#order_email").css("borderColor","#FDB6B6");
+  send_order_email = '0';   
+ }else { $("#order_email").css("borderColor","#DBDBDB"); send_order_email = '1';}
+  
+ // Проверка телефона
+ 
+  if (order_phone == "" || order_phone.length > 50)
+ {
+    $("#order_phone").css("borderColor","#FDB6B6");
+    send_order_phone = '0';   
+ }else { $("#order_phone").css("borderColor","#DBDBDB"); send_order_phone = '1';}
+ 
+ // Проверка Адресса
+ 
+  if (order_address == "" || order_address.length > 150)
+ {
+    $("#order_address").css("borderColor","#FDB6B6");
+    send_order_address = '0';   
+ }else { $("#order_address").css("borderColor","#DBDBDB"); send_order_address = '1';}
+  
+} 
+ // Глобальная проверка
+ if (send_order_delivery == "1" && send_order_fio == "1" && send_order_email == "1" && send_order_phone == "1" && send_order_address == "1")
+ {
+    // Отправляем форму
+   return true;
+ }
+
+e.preventDefault();
+
+});
+
 });
