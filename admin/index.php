27c52c86beaa3fb427bd53607ba4960101b2906c
Yan <rewert60@gmail.com>
2015-11-25 23:59:21 +0200

    html and css markup

---
+++ admin/index.php
@@ -0,0 +1,54 @@
+<?php
+	session_start();
+    
+    if ($_SESSION['auth_admin'] == "yes_auth")
+    {
+define('myyshop', true);
+    
+    if (isset($_GET["logout"]))
+    {
+        unset($_SESSION["auth_admin"]);
+        header("Location: login.php");
+    }
+    
+    $_SESSION['urlpage'] = "<a href='index.php'>Главная</a>";
+    
+    include("include/db_connect.php");
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />    
+
+	<title>Панель управления</title>
+</head>
+
+<body>
+
+<div id="block-body">
+<?php
+	include("include/block-header.php");
+?>
+<div id="block-content">
+<div id="block-parameters">
+<p id="title-page">Общая статистика</p>
+
+</div>
+
+</div>
+</div>
+
+
+
+</body>
+</html>
+
+<?php
+} else
+{
+    header("Location: login.php");
+}	
+?>
+    
