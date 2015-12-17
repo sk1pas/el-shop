0724791893fbf53135a06e6d638542eede6f524a
Yan <rewert60@gmail.com>
2015-12-17 23:33:41 +0200

    View orders

---
+++ admin/orders.php
@@ -0,0 +1,146 @@
+<?php
+	session_start();
+    
+    if ($_SESSION['auth_admin'] == "yes_auth")
+    {
+define('myyshop', true);
+    
+    if (isset($_GET["logout"]))
+    {
+        unset($_SESSION["auth_admin"]);
+        header("Location: login.php");
+    }
+    
+    $_SESSION['urlpage'] = "<a href='index.php'>Главная</a> \ <a href='orders.php'>Заказы</a>";
+    
+    include("include/db_connect.php");
+    include("include/functions.php"); 
+    
+    $id = clear_string($_GET["id"]);
+    $sort = $_GET["sort"];
+    switch ($sort) {
+        case 'all-orders':
+        
+        $sort = "order_id DESC";
+        $sort_name = 'От А до Я';
+        
+        break;
+        
+        case 'confirmed':
+        
+        $sort = "order_confirmed = 'yes' DESC";
+        $sort_name = 'Обработанные';
+        
+        break;
+        
+        case 'no-confirmed':
+        
+        $sort = "order_confirmed = 'no' DESC";
+        $sort_name = 'Не обработанные';
+        
+        break;
+        
+        default:
+        
+        $sort = "order_id DESC";
+        $sort_name = 'От А до Я';
+        
+        break;       
+    }
+  
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="jquery_confirm/jquery_confirm.css" rel="stylesheet" type="text/css" />      
+    <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script> 
+    <script type="text/javascript" src="js/script.js"></script> 
+    <script type="text/javascript" src="jquery_confirm/jquery_confirm.js"></script> 
+
+	<title>Панель управления - Заказы</title>
+</head>
+
+<body>
+
+<div id="block-body">
+<?php
+	include("include/block-header.php");  
+    
+    $all_count = mysql_query("SELECT * FROM orders",$link); 
+    $all_count_result = mysql_num_rows($all_count);
+    
+    $buy_count = mysql_query("SELECT * FROM orders WHERE order_confirmed = 'yes'",$link); 
+    $buy_count_result = mysql_num_rows($buy_count);
+    
+    $no_buy_count = mysql_query("SELECT * FROM orders WHERE order_confirmed = 'no'",$link); 
+    $no_buy_count_result = mysql_num_rows($no_buy_count);
+?>
+<div id="block-content">
+
+<div id="block-parameters">
+
+<ul id="options-list">
+<li>Сортировать</li>
+<li><a id="select-links" href="#"><? echo $sort_name; ?></a>
+
+<ul id="list-links-sort">
+<li><a href="orders.php?sort=all-orders">От А до Я</a></li>
+<li><a href="orders.php?sort=confirmed">Обработанные</a></li>
+<li><a href="orders.php?sort=no-confirmed">Не обработанные</a></li>
+</ul>
+
+</li>
+</ul>
+</div>
+
+<div id="block-info">
+
+<ul id="review-info-count">
+<li>Всего заказов - <strong><?php echo $all_count_result;?></strong></li>
+<li>Обработанных - <strong><?php echo $buy_count_result;?></strong></li>
+<li>Не обработанных - <strong><?php echo $no_buy_count_result;?></strong></li>
+</ul>
+</div>
+
+<?php
+	$result = mysql_query("SELECT * FROM orders ORDER BY $sort",$link);
+    
+    if(mysql_num_rows($result) > 0)
+    {
+        $row = mysql_fetch_array($result);
+        do
+        {
+            if($row["order_confirmed"] == 'yes')
+            {
+                $status = '<span class="green">Обработан</span>';
+            } else
+            {
+                $status = '<span class="red">Не обработан</span>';
+            }
+            
+            echo '
+            <div class="block-order">
+            
+            <p class="order-datetime" >'.$row["order_datetime"].'</p>
+            <p class="order-number">Заказ № '.$row["order_id"].' - '.$status.'</p>
+            <p class="order-link"><a class="green" href="view_order.php?id='.$row["order_id"].'">Подробнее</a></p>
+            </div>            
+            ';
+        } while ($row = mysql_fetch_array($result));
+    }
+?>
+
+</div>
+</div>
+</body>
+</html>
+
+<?php
+} else
+{
+    header("Location: login.php");
+}	
+?>
--- cart.php
+++ cart.php
@@ -22,14 +22,64 @@
     
     if (isset($_POST["submitdata"]))
     {
-        $_SESSION["order_delivery"] = $_POST["order_delivery"];
-        $_SESSION["order_fio"] = $_POST["order_fio"];
-        $_SESSION["order_email"] = $_POST["order_email"];
-        $_SESSION["order_phone"] = $_POST["order_phone"];
-        $_SESSION["order_address"] = $_POST["order_address"];
-        $_SESSION["order_note"] = $_POST["order_note"];
+    if ( $_SESSION['auth'] == 'yes_auth' ) 
+ {
         
-        header("Location: cart.php?action=completion");
+    mysql_query("INSERT INTO orders(order_datetime,order_dostavka,order_fio,order_address,order_phone,order_note,order_email)
+						VALUES(	
+                             NOW(),
+                            '".$_POST["order_delivery"]."',					
+							'".$_SESSION['auth_surname'].' '.$_SESSION['auth_name'].' '.$_SESSION['auth_patronymic']."',
+                            '".$_SESSION['auth_address']."',
+                            '".$_SESSION['auth_phone']."',
+                            '".$_POST['order_note']."',
+                            '".$_SESSION['auth_email']."'                              
+						    )",$link);         
+
+ }else
+ {
+$_SESSION["order_delivery"] = $_POST["order_delivery"];
+$_SESSION["order_fio"] = $_POST["order_fio"];
+$_SESSION["order_email"] = $_POST["order_email"];
+$_SESSION["order_phone"] = $_POST["order_phone"];
+$_SESSION["order_address"] = $_POST["order_address"];
+$_SESSION["order_note"] = $_POST["order_note"];
+
+    mysql_query("INSERT INTO orders(order_datetime,order_dostavka,order_fio,order_address,order_phone,order_note,order_email)
+						VALUES(	
+                             NOW(),
+                            '".clear_string($_POST["order_delivery"])."',					
+							'".clear_string($_POST["order_fio"])."',
+                            '".clear_string($_POST["order_address"])."',
+                            '".clear_string($_POST["order_phone"])."',
+                            '".clear_string($_POST["order_note"])."',
+                            '".clear_string($_POST["order_email"])."'                   
+						    )",$link);    
+ }
+
+                          
+ $_SESSION["order_id"] = mysql_insert_id();                          
+                            
+$result = mysql_query("SELECT * FROM cart WHERE cart_ip = '{$_SERVER['REMOTE_ADDR']}'",$link);
+If (mysql_num_rows($result) > 0)
+{
+$row = mysql_fetch_array($result);    
+
+do{
+
+    mysql_query("INSERT INTO buy_products(buy_id_order,buy_id_product,buy_count_product)
+						VALUES(	
+                            '".$_SESSION["order_id"]."',					
+							'".$row["cart_id_product"]."',
+                            '".$row["cart_count"]."'                   
+						    )",$link);
+
+
+
+} while ($row = mysql_fetch_array($result));
+}
+                            
+header("Location: cart.php?action=completion");
     }
     
     $result = mysql_query("SELECT * FROM cart,table_products WHERE cart.cart_ip = '{$_SERVER['REMOTE_ADDR']}' AND table_products.products_id = cart.cart_id_product", $link);
